2227dd038165af9cff477fdadf40ca2faafbcc06
Hardik Kumar <kumarhardik821@gmail.com>
2025-01-17 02:54:26 -0800

    a new project to learn java script

---
+++ toast.js
@@ -0,0 +1,8 @@
+let submit = document.querySelector(".submit");
+let error = document.querySelector(".error");
+let invlid = document.querySelector(".invalid");
+
+const showtoast = (msg)=>{
+    let toast = document.createElement('div');
+    
+}
